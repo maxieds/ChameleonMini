f9391af0bb9a4fbcbf0dbe01e739a334297aeba2
Dr. Maxie Dion Schmidt <maxieds@Drs-MacBook-Pro.local>
2026-02-05 14:49:42 -0600

    Stashing incremental work on custom DESFire tag build configurations II

---
+++ Firmware/Chameleon-Mini/Application/CryptoAESExtended.c
@@ -0,0 +1,34 @@
+/*
+The DESFire stack portion of this firmware source
+is free software written by Dr. Maxie Dion Schmidt (@maxieds):
+You can redistribute it and/or modify
+it under the terms of this license.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+The complete source distribution of
+this firmware is available at the following link:
+https://github.com/maxieds/ChameleonMiniFirmwareDESFireStack.
+
+Based in part on the original DESFire code created by
+@dev-zzo (GitHub handle) [Dmitry Janushkevich] available at
+https://github.com/dev-zzo/ChameleonMini/tree/desfire.
+
+This notice must be retained at the top of all source files where indicated.
+*/
+
+/*
+ * CryptoAESExtended.h :
+ * Maxie D. Schmidt (github.com/maxieds)
+ */
+
+/*
+ * NOTE: These (two) source files implement software-based AES-192 and 
+ *       AES-256 encryption routines. The source is heavily based on the 
+ *       project (with the top level repo "unlicense.txt" license): 
+ *       https://github.com/kokke/tiny-AES-c
+ */
+
+
