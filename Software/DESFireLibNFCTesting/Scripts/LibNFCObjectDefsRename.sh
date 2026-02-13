b76b6f6f404f5a6b6f855b661b4d60aa07f2c1ef
Dr. Maxie Dion Schmidt <maxieds@Drs-MacBook-Pro.local>
2026-02-13 13:37:50 -0600

    Debugging LibNFC/DESFire test code - currently driver issues on MacOS

--- Software/DESFireLibNFCTesting/Scripts/LibNFCObjectDefsRename.sh
+++ Software/DESFireLibNFCTesting/Scripts/LibNFCObjectDefsRename.sh
@@ -5,7 +5,9 @@ if [[ "$PLATFORM" != "Darwin" ]]; then
 	exit 0
 fi
 
+# NOTE: Run ``brew install coreutils binutils``
 OBJCOPY=/usr/local/opt/binutils/bin/objcopy
+#OBJCOPY=$(which objcopy)
 archiveFile=$(greadlink -f $1)
 
 $OBJCOPY --redefine-sym _nfc_device_pnd_set_property_bool=nfc_device_pnd_set_property_bool $archiveFile
